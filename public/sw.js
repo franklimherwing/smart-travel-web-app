7cf5fe86f946c9d7dc4c0facf82c1fe23c393fee
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-09-18 13:41:19 -0600

    Bump service worker cache to v0.4.4

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smart-travel-v0.4.3';
+const CACHE = 'smart-travel-v0.4.4';
 const CORE = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', event => {
