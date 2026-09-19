ce5651d5e75250e1feb15222b5bb4675969dd79f
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-09-18 20:42:40 -0600

    v0.6.1 refresh cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smart-travel-v0.6.0';
+const CACHE = 'smart-travel-v0.6.1';
 const CORE = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', event => {
