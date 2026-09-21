818a3836ee5e3e706150bff2542636fa7a89cbed
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-09-20 22:16:20 -0600

    Bump app to v0.10.1

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smart-travel-v0.10.0';
+const CACHE = 'smart-travel-v0.10.1';
 const CORE = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', event => {
